d6aa2401ef9d1068a83c411b7eb3326051f040df
Agustin Carrasco <asermax@gmail.com>
2018-08-05 02:25:12 -0300

    feat(styles): added media query breakpoints helper

--- web/src/styles/util.js
+++ web/src/styles/util.js
@@ -1,7 +1,16 @@
+import * as R from 'ramda'
 import { css } from 'emotion'
 
+const breakpoints = {
+  mobile: '40rem',
+  tablet: '80rem',
+  deskotp: '120rem',
+}
+
+export const mq = R.map((value) => `@media (min-width: ${value})`)(breakpoints)
+
 export const hideOnMobile = css`
-  @media (max-width: 480px) {
+  @media (max-width: ${breakpoints.mobile}) {
     display: none;
   }
 `
