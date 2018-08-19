d538f8b78f6584d5ae173e21e34bca8d06ae5cb6
Agustin Carrasco <asermax@gmail.com>
2018-08-19 20:38:31 -0300

    feat(order): added order and order product models

---
+++ api/src/consumer/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Order(models.Model):
+    user = models.CharField(max_length=200)
+    products = models.ManyToManyField('producer.Product', through='OrderProduct')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Orden de {}'.format(self.user)
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    product = models.ForeignKey('producer.Product', on_delete=models.CASCADE, related_name='+')
+    amount = models.PositiveIntegerField()
