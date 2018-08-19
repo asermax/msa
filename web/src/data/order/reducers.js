0b3ef27426c31d1c0b3d4e359e83ef46faa9f1c3
Agustin Carrasco <asermax@gmail.com>
2018-08-19 19:30:42 -0300

    fix(order): remove order products with amount 0

--- web/src/data/order/reducers.js
+++ web/src/data/order/reducers.js
@@ -6,7 +6,18 @@ const productsDefault = {}
 const products = (state = productsDefault, action) => {
   switch (action.type) {
     case SET_ORDER_PRODUCT_AMOUNT:
-      return R.assoc(action.id, action.amount)(state)
+      return R.ifElse(
+        R.compose(R.equals(0), R.prop('amount')),
+        R.compose( // in case the amount is 0
+          R.dissoc, // remove the product id from the order products
+          R.prop('id'),
+        ),
+        R.compose( // in case the value is not 0
+          R.apply(R.assoc), // associate the amount to the modified order product id
+          R.props([ 'id', 'amount' ]),
+        ),
+      )(action)(state)
+
     default:
       return state
   }
