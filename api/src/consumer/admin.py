9b138a4bbafaa30d6c876cffd903e1a6e0ff047d
Agustin Carrasco <asermax@gmail.com>
2018-08-19 21:08:24 -0300

    feat(order): register to django's admin

--- api/src/consumer/admin.py
+++ api/src/consumer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class OrderProductInline(admin.TabularInline):
+    model = models.OrderProduct
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model = models.Order
+    inlines = (OrderProductInline,)
+
+
+admin.site.register(models.Order, OrderAdmin)
