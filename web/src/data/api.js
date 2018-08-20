76ae2c887a854ca46880760c3bd19040a589293c
Agustin Carrasco <asermax@gmail.com>
2018-08-19 23:34:02 -0300

    feat(api): added extra wretches for modifications api calls

--- web/src/data/api.js
+++ web/src/data/api.js
@@ -1,15 +1,28 @@
 import wretch from 'wretch'
+import cookie from 'js-cookie'
 
-export const PRODUCT_ENTRYPOINT = 'entrypoint/product'
+export const PRODUCT_ENTRYPOINT = 'api/entrypoint/product'
+export const ORDER_ENTRYPOINT = 'api/entrypoint/order'
 
 const baseWretch = wretch()
   .url('http://localhost/api')
 
+const modificationWretch = baseWretch
+  .headers({
+    'X-CSRFToken': cookie.get('csrftoken'),
+  })
+
 const entrypointsMap = {
   [PRODUCT_ENTRYPOINT]: '/products/',
+  [ORDER_ENTRYPOINT]: '/orders/',
 }
 
 export const apiGet = (entrypoint) => baseWretch
   .url(entrypointsMap[entrypoint])
   .get()
+  .json()
+
+export const apiPost = (entrypoint, data) => modificationWretch
+  .url(entrypointsMap[entrypoint])
+  .post(data)
   .json()
