ed9234f2880cf24bc21366823f1db458518cb118
Agustin Carrasco <asermax@gmail.com>
2018-08-19 23:33:26 -0300

    fix(order): nested writable order product

--- api/src/consumer/serializers.py
+++ api/src/consumer/serializers.py
@@ -3,23 +3,25 @@ from rest_framework import serializers
 from . import models
 
 
-class OrderProductSerializer(serializers.ModelSerializer):
+class NestedOrderProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.OrderProduct
-        fields = ('id', 'order', 'product', 'amount')
+        fields = ('id', 'product', 'amount')
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    products = NestedOrderProductSerializer(many=True)
+
     class Meta:
         model = models.Order
         fields = ('id', 'user', 'products', 'created')
         read_only_fields = ('created',)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def create(self, validated_data):
+        products = validated_data.pop('products')
+        order = models.Order.objects.create(**validated_data)
+        models.OrderProduct.objects.bulk_create([
+            models.OrderProduct(order=order, **product) for product in products
+        ])
 
-        if self.instance is not None:
-            print(self.fields, self.fields['products'])
-            # if we have an instance to modify, we want to allow nested modifications
-            self.fields['products'] = OrderProductSerializer(many=True, write_only=True)
-
+        return order
