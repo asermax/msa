bc67dd2d145a95709c3416a3d8e69b6fb6db8897
Agustin Carrasco <asermax@gmail.com>
2018-08-23 00:20:56 -0300

    chore(flex): removed unused component

--- web/src/components/Flex.jsx
+++
@@ -1,8 +0,0 @@
-import * as R from 'ramda'
-import styled from 'react-emotion'
-
-export const Flex = styled.div`
-  display: flex;
-  flex-direction: ${R.propOr('row', 'direction')};
-  align-items: ${R.propOr('start', 'aligment')};
-`
