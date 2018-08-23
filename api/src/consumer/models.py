235832339a9f868ab69693476959fcb213f5ee67
Agustin Carrasco <asermax@gmail.com>
2018-08-22 23:31:46 -0300

    fix(order): don't use a many to many field, use order product directly

--- api/src/consumer/models.py
+++ api/src/consumer/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Order(models.Model):
     user = models.CharField(max_length=200)
-    products = models.ManyToManyField('producer.Product', through='OrderProduct')
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -11,6 +10,6 @@ class Order(models.Model):
 
 
 class OrderProduct(models.Model):
-    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='products')
     product = models.ForeignKey('producer.Product', on_delete=models.CASCADE, related_name='+')
     amount = models.DecimalField(max_digits=5, decimal_places=2)
