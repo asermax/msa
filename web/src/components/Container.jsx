daa79df6f7752d634b24f2eb161f453225146426
Agustin Carrasco <asermax@gmail.com>
2018-08-26 21:36:08 -0300

    fix(container): added vertical padding

--- web/src/components/Container.jsx
+++ web/src/components/Container.jsx
@@ -3,7 +3,7 @@ import styled from 'react-emotion'
 export const Container = styled.div`
   margin: 0 auto;
   max-width: 112rem;
-  padding: 0 2rem;
+  padding: 2rem;
   position: relative;
   width: 100%;
 `
