3aeb446578237a7809a95ae8434f17baa9063dcf
Agustin Carrasco <asermax@gmail.com>
2018-09-02 19:03:32 -0300

    style(forRoute): test flow on the forRoute hoc

--- web/src/hocs/forRoute.js
+++ web/src/hocs/forRoute.js
@@ -1,4 +1,6 @@
 /* global process */
+// @flow
+import type { ComponentType } from 'react'
 import * as R from 'ramda'
 import { connect } from 'react-redux'
 import { compose, branch, renderNothing, setDisplayName, wrapDisplayName } from 'recompose'
@@ -8,7 +10,7 @@ const mapStateToProps = (state) => ({
   currentRoute: getCurrentRoute(state),
 })
 
-export const forRoute = (route) => {
+export const forRoute = (route: string) => {
   const hoc = compose(
     connect(mapStateToProps),
     branch(
@@ -18,7 +20,7 @@ export const forRoute = (route) => {
   )
 
   if (process.env.NODE_ENV !== 'production') {
-    return BaseComponent =>
+    return (BaseComponent: ComponentType<any>) =>
       setDisplayName(
         wrapDisplayName(BaseComponent, 'forRoute'),
       )(hoc(BaseComponent))
