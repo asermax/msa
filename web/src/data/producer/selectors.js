1bb6d8ef010340a8ea1e7b3332f8c8b74bf08060
Agustin Carrasco <asermax@gmail.com>
2018-09-24 00:14:55 -0300

    fix(producer): selectors were copied from producer, fixed and added more

--- web/src/data/producer/selectors.js
+++ web/src/data/producer/selectors.js
@@ -1,16 +1,33 @@
 // @flow
 import type { State } from 'data/types'
-import type { Product, ProductsById } from './types'
+import type { Producer, ProducersIds, ProducersById } from './types'
 import * as R from 'ramda'
 import createCachedSelector from 're-reselect'
 
-export const getProductIds: (State) => Array<string> = R.path([ 'product', 'ids' ])
-export const getProductsById: (State) => ProductsById = R.path([ 'product', 'byId' ])
-export const getProduct: (State, id: string) => Product =
-  createCachedSelector<State, any, ProductsById, Product>(
+export const getProducersIds: (State) => ProducersIds = R.compose(
+  R.prop('ids'),
+  R.prop('producer'),
+)
+export const getProducersById: (State) => ProducersById = R.compose(
+  R.prop('byId'),
+  R.prop('producer'),
+)
+export const getProducer: (State, id: string) => Producer =
+  createCachedSelector<State, any, ProducersById, Producer>(
     [
-      R.nthArg(1), // product id
-      getProductsById, // product map
+      R.nthArg(1), // producer id
+      getProducersById, // producer map
     ],
-    R.prop, // receives (id, productMap)
+    R.prop, // receives (id, producerMap)
+  )(R.nthArg(1))  // memoize by id
+export const getProducerBySlug: (State, slug: string) => ?Producer =
+  createCachedSelector<State, any, ProducersById, ?Producer>(
+    [
+      R.nthArg(1), // producer slug
+      getProducersById, // producer map
+    ],
+    (slug, producers) => R.compose(
+      R.find(R.propEq('slug', slug)), // find by the slug
+      R.values,
+    )(producers),
   )(R.nthArg(1))  // memoize by id
