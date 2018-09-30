ab4004842e71088fb2c27f3a5aab2eec001f1ceb
Agustin Carrasco <asermax@gmail.com>
2018-09-29 22:53:59 -0300

    refactor(order): use the current operative when creating a new order

--- api/src/consumer/views.py
+++ api/src/consumer/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from operative import models as operative_models
 from . import models, serializers, filters
 
 
@@ -7,3 +8,8 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = models.Order.objects.all()
     serializer_class = serializers.OrderSerializer
     filterset_class = filters.OrderFilterSet
+
+    def perform_create(self, serializer):
+        # use the current operative when creating a new order
+        current_operative = operative_models.Operative.objects.get_current()
+        serializer.save(operative=current_operative)
