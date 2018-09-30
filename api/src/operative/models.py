7191fdca8cc197d13b9f6bf865933b835c06a61c
Agustin Carrasco <asermax@gmail.com>
2018-09-29 22:53:32 -0300

    feat(operative): added custom queryset method to obtain the current operative

--- api/src/operative/models.py
+++ api/src/operative/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.utils import formats
 
 
+class OperativeQueryset(models.QuerySet):
+    def get_current(self):
+        return self.latest()
+
+
 class Operative(models.Model):
     start_date = models.DateField(auto_now_add=True, verbose_name='fecha de inicio')
     ordering_limit_date = models.DateField(verbose_name='fecha límite de ordenes')
 
+    objects = OperativeQueryset.as_manager()
+
     class Meta:
+        get_latest_by = ('-start_date',)
         verbose_name = 'operativo'
 
     def __str__(self):
