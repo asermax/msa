bedaccd98d390ebad5ad327fe0a9ce6e41a5ee5b
Agustin Carrasco <asermax@gmail.com>
2018-10-07 00:41:07 -0300

    refactor(childrenToSlots): use positional order to determine slots

--- web/src/hocs/childrenToSlots.js
+++ web/src/hocs/childrenToSlots.js
@@ -6,16 +6,13 @@ import { compose, mapProps, setDisplayName, wrapDisplayName } from 'recompose'
 export const childrenToSlots = (slots) => {
   const hoc = compose(
     mapProps(({ children, ...props }) => {
-      const childrenArray = Children.toArray(children)
-
       return R.compose(
-        (slots) => Object.assign(props, { slots }),
+        R.merge(props),
+        R.objOf('slots'),
         R.fromPairs,
-        R.map((slot) => ([
-          slot,
-          R.find(R.compose(R.propEq('slot', slot), R.prop('props')))(childrenArray),
-        ])),
-      )(slots)
+        R.zip(slots), // zip them in order with slots
+        R.partialRight(Children.map, [ R.identity ]), // return immediate children
+      )(children)
     }),
   )
 
