0a9b4db6e5e9c0e264d6b90b7b6a18c4287a50a3
Agustin Carrasco <asermax@gmail.com>
2018-10-07 00:25:41 -0300

    refactor(storybook): added wrapper around stories to add some spacing

--- web/.storybook/config.js
+++ web/.storybook/config.js
@@ -1,5 +1,18 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
 import 'styles/global'
+import styled from 'react-emotion'
+
+const Wrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`
+
+addDecorator((story) => (
+  <Wrapper>{story()}</Wrapper>
+))
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.js$/)
