c3c3f889c0d44f864e4fd1d95ac04b1ef7bca999
Agustin Carrasco <asermax@gmail.com>
2018-10-08 22:34:14 -0300

    refactor(forRoute): allow to send more than one route

--- web/src/hocs/forRoute.js
+++ web/src/hocs/forRoute.js
@@ -1,5 +1,6 @@
 /* global process */
 import * as R from 'ramda'
+import * as RA from 'ramda-adjunct'
 import { connect } from 'react-redux'
 import { compose, branch, renderNothing, setDisplayName, wrapDisplayName } from 'recompose'
 import { getCurrentRoute } from 'data/route/selectors'
@@ -8,11 +9,16 @@ const mapStateToProps = (state) => ({
   currentRoute: getCurrentRoute(state),
 })
 
-export const forRoute = (route) => {
+export const forRoute = (routes) => {
+
   const hoc = compose(
     connect(mapStateToProps),
     branch(
-      R.compose(R.not, R.propEq('currentRoute', route)),
+      R.compose(
+        R.not,
+        R.contains(R.__, R.unless(RA.isArray, R.of)(routes)),
+        R.prop('currentRoute'),
+      ),
       renderNothing,
     ),
   )
