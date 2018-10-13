283d98793e6a0092f10a373c08f7c38f96b61d6b
Agustin Carrasco <asermax@gmail.com>
2018-10-13 19:29:14 -0300

    refactor(tabs): change the styling of active/inactive tabs

--- web/src/components/Tabs/Tab.jsx
+++ web/src/components/Tabs/Tab.jsx
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types'
 import styled from 'react-emotion'
+import { transparentize } from 'polished'
 import { colors } from 'styles/util'
 
 export const Tab = styled.div`
   padding: 0 2rem;
   margin-right: 1rem;
   height: 3rem;
-  border: 0.1rem solid ${colors.primary};
-  border-bottom: 0;
+  border-width: ${({ active }) => active ? 0.1 : 0}rem;
+  border-style: solid;
+  border-color: ${colors.primary};
+  border-bottom-width: ${({ active }) => active ? 0 : 0.1}rem;
   border-radius: 0.4rem 0.4rem 0 0;
-  background-color: ${({ active }) => active ? colors.primary : colors.transparent};
-  color: ${({ active }) => active ? colors.white : colors.primary};
+  background-color: ${({ active }) => active ? colors.white : transparentize(0.5, colors.primary)};
+  color: ${({ active }) => active ? colors.primary : colors.white};
   font-size: 1.1rem;
   font-weight: 700;
   letter-spacing: 0.1rem;
@@ -20,7 +23,7 @@ export const Tab = styled.div`
 
   &:hover, &:focus {
     // if used as a link, avoid breaking on hover
-    color: ${({ active }) => active ? colors.white : colors.primary};
+    color: ${({ active }) => active ? colors.primary : colors.white};
   }
 `
 
--- web/src/components/Tabs/TabContainer.jsx
+++ web/src/components/Tabs/TabContainer.jsx
@@ -2,9 +2,9 @@ import styled from 'react-emotion'
 import { colors } from 'styles/util'
 
 export const TabContainer = styled.div`
+  height: 2.95rem;
   border-bottom: 0.1rem solid ${colors.primary};
   margin-bottom: 1rem;
   display: flex;
   flex-direction: row;
-  box-shadow: 0
 `
