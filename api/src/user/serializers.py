f9209480582df3b5b37292a7c8762636373ff295
Agustin Carrasco <asermax@gmail.com>
2018-10-21 03:54:08 -0300

    refactor(session): had to manually do a bunch of stuff for login to work

--- api/src/user/serializers.py
+++ api/src/user/serializers.py
@@ -4,13 +4,27 @@ from social_django import utils as social_utils
 
 
 class SessionSerializer(serializers.Serializer):
-    auth_token = serializers.CharField()
+    code = serializers.CharField()
 
     def validate(self, data):
         # validate the user against the social backend
         social_strategy = social_utils.load_strategy()
         backend = social_utils.load_backend(social_strategy, 'google-oauth2', None)
-        user = backend.do_auth(data['auth_token'])
+        backend.data = data
+        backend.redirect_uri = social_strategy.get_setting(
+            'SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI'
+        )
+        print(backend.redirect_uri)
+        params = backend.auth_complete_params()
+        response = backend.request_access_token(
+            backend.access_token_url(),
+            data=params,
+            headers=backend.auth_headers(),
+            auth=backend.auth_complete_credentials(),
+            method=backend.ACCESS_TOKEN_METHOD
+        )
+        backend.process_error(response)
+        user = backend.do_auth(response['access_token'], response=response)
 
         if not user:
             raise serializers.ValidationError('bad token')
@@ -25,8 +39,12 @@ class SessionSerializer(serializers.Serializer):
         # login the user
         auth.login(request, user)
 
-        # return the user data serialized
-        return UserSerializer(user).data
+        # return the user
+        return user
+
+    def to_representation(self, instance):
+        # the instance of this serializer is actually an user (once it got created)
+        return UserSerializer(instance).data
 
 
 class UserSerializer(serializers.ModelSerializer):
