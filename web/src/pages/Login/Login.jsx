af6d8a889037e2f2db07f5c8cff136aa3e5b71a6
Agustin Carrasco <asermax@gmail.com>
2018-10-21 17:53:07 -0300

    refactor(login): use styled button component to show the google login button

--- web/src/pages/Login/Login.jsx
+++ web/src/pages/Login/Login.jsx
@@ -6,7 +6,9 @@ import { env } from 'utils'
 import { LOGIN } from 'data/route/actions'
 import { createSession } from 'data/user/actions'
 import { GoogleLogin } from 'react-google-login'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Centered } from 'components/Centered'
+import { Button } from 'components/Button'
 
 const mapDispatchToProps = (dispatch) => ({
   login: ({ code }) => dispatch(createSession(code)),
@@ -18,17 +20,22 @@ const enhancer = compose(
   setDisplayName('Login'),
 )
 
+const StyledGoogleLogin = Button.withComponent(GoogleLogin)
+
 export const Login = enhancer(({ login }) => (
   <Centered>
-    <GoogleLogin
+    <StyledGoogleLogin
       clientId={env.GOOGLE_OAUTH_CLIENT_ID}
       scope="openid email profile"
       accessType="offline"
       responseType="code"
       uxMode="popup"
       onSuccess={login}
+      outlined
     >
-      Login
-    </GoogleLogin>
+      <FontAwesomeIcon icon={[ 'fab', 'google' ]}/>
+      &nbsp;
+      Ingresar
+    </StyledGoogleLogin>
   </Centered>
 ))
