5f8947d9f59dc38cbdda88a7fa00c1c58894c1a9
Agustin Carrasco <asermax@gmail.com>
2018-11-03 23:31:21 -0300

    refactor(modal close button): make the icon larger, fix types

--- web/src/components/Modal/ModalCloseButton.jsx
+++ web/src/components/Modal/ModalCloseButton.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { compose, setPropTypes, setDisplayName } from 'recompose'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as styles from './styles'
 
-const enhancer = compose(
-  setPropTypes({
-    onClose: PropTypes.func.isRequired,
-  }),
-  setDisplayName('ModalCloseButton'),
+export const ModalCloseButton = ({ onClose }) => (
+  <div
+    onClick={onClose}
+  >
+    <FontAwesomeIcon
+      icon="times"
+      size="lg"
+      css={styles.close}
+    />
+  </div>
 )
 
-export const ModalCloseButton = enhancer(({ onClose }) => (
-  <FontAwesomeIcon
-    icon="times"
-    size="lg"
-    css={styles.close}
-    onClick={onClose}
-  />
-))
+ModalCloseButton.propTypes = {
+  onClose: PropTypes.func.isRequired,
+}
