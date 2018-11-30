0603fe2c1f5db3a40d323e7405de1b9900d84389
Agustin Carrasco <asermax@gmail.com>
2018-11-30 03:18:57 -0300

    feat(query params): allow to set query params through a dummy action

--- web/src/data/route/middlewares.js
+++ web/src/data/route/middlewares.js
@@ -1,11 +1,12 @@
 import * as R from 'ramda'
-import { ALL_ROUTES } from './actions'
-import { getStickyQueryParameters } from './selectors'
+import { ALL_ROUTES, SET_ROUTE_QUERY_PARAM } from './actions'
+import { getQuery, getStickyQueryParameters, getCurrentRoute } from './selectors'
 
 export const queryParametersMiddleware = R.curry((store, next, action) => {
   if (R.contains(action.type)(ALL_ROUTES)) {
     const stickyQueryParams = getStickyQueryParameters(store.getState())
     const queryLens = R.lensPath([ 'payload', 'query' ])
+
     action = R.over(
       queryLens,
       R.compose(
@@ -13,6 +14,21 @@ export const queryParametersMiddleware = R.curry((store, next, action) => {
         R.defaultTo({}),
       ),
     )(action)
+  } else if (action.type === SET_ROUTE_QUERY_PARAM) {
+    const currentRoute = getCurrentRoute(store.getState())
+    const queryParams = getQuery(store.getState())
+
+    const { name, sticky, value } = action.payload
+    const parameterName = `${name}${sticky ? '!' : ''}`
+    const change = R.isEmpty(value) ? R.dissoc(parameterName) : R.assoc(parameterName, value)
+    const newParams = change(queryParams)
+
+    action = {
+      type: currentRoute,
+      payload: {
+        query: newParams,
+      },
+    }
   }
 
   next(action)
